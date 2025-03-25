5572c58a254d95ae63c3711f0a977dc8756d69ae
Daniel Collins <luckypuppydc@gmail.com>
2025-03-25 13:21:05 +0700

    added save/load and removed debugging

---
+++ weapons.py
@@ -0,0 +1,19 @@
+class Sword:
+    def __init__(self):
+        self.attack_bonus = 4
+        self.min_attack = 8
+
+class Greatsword:
+    def __init__(self):
+        self.attack_bonus = 6
+        self.min_attack = 12
+
+class Chainmail:
+    def __init__(self):
+        self.defense_bonus = 4
+        self.min_defense = 8
+
+class PlateArmor:
+    def __init__(self):
+        self.defense_bonus = 6
+        self.min_defense = 12
